d07c66d4740961d22def95fd13797bf2f92d6913
Milani Yogeswaran <milaniyoges@gmail.com>
2023-07-03 08:36:30 +0530

    chnages home, contactus

---
+++ frontend/src/components/pages/Events.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Events() {
+    return (
+        <div className="EventComponent" id="EventComponent">
+            <h1 className="h1-heading">Events</h1>
+        </div>
+    )
+}
+
+export default Events;
