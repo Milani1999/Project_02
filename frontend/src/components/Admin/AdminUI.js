06ce756d1c944a63d32f44f14a461a5534fda1ad
Milani Yogeswaran <milaniyoges@gmail.com>
2023-07-07 13:41:58 +0530

    Link, route, browser route added

---
+++ frontend/src/components/Admin/AdminUI.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function AdminUI() {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+            <h1>Admin Dashboard</h1>
+        </div>
+    )
+}
+
+export default AdminUI;
