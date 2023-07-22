f57cfdee9396220a36d0cea7cb4de661ee5dc3a7
SubashiDeSilva <sudharmasubhashini@gmail.com>
2023-07-22 21:33:35 +0530

    Login Function Backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,49 @@
-const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+const Student = require("../models/studentModel");
+const Staff = require("../models/staffModel");
+const Admin = require("../models/adminModel");
 const generateToken = require('../utils/generateToken');
 
 const authUser = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
+  if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+    const existingAdmin = await Admin.findOne({ username });
 
-  if (user && (await user.matchPassword(password))) {
+    if (!existingAdmin) {
+      const defaultAdmin = new Admin({
+        username: process.env.ADMIN_USERNAME,
+        password: process.env.ADMIN_PASSWORD,
+        role: process.env.ADMIN_ROLE,
+      });
+
+      await defaultAdmin.save();
+    }
+
     res.json({
-      _id: user._id,
-      name: user.name,
-      picture: user.picture,
-      token: generateToken(user._id),
-    })
+      role: process.env.ADMIN_ROLE,
+      token: generateToken("admin"),
+    });
+    return;
   }
-  else {
-    res.status(400)
+
+  const student = await Student.findOne({ username });
+  const staff = await Staff.findOne({ username });
+
+  if (student && (await student.matchPassword(password))) {
+    res.json({
+      name: student.fullname,
+      role: student.role,
+      token: generateToken(student._id),
+    });
+  } else if (staff && (await staff.matchPassword(password))) {
+    res.json({
+      name: staff.fullname,
+      role: staff.role,
+      token: generateToken(staff._id),
+    });
+  } else {
+    res.status(401);
     throw new Error('Invalid username or password');
   }
 });
---
+++ backend/models/adminModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        default: process.env.ADMIN_USERNAME,
+    },
+    password: {
+        type: String,
+        default: process.env.ADMIN_PASSWORD,
+    },
+    role: {
+        type: String,
+        default: process.env.ADMIN_ROLE,
+    },
+}, {
+    timestamps: true,
+});
+
+adminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+const Admin = new mongoose.model("Admin", adminSchema)
+
+module.exports = Admin;
