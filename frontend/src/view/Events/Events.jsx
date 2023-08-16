06553d9835d59809b53b787ff4fc28757977c926
Milani Yogeswaran <milaniyoges@gmail.com>
2023-08-16 23:34:59 +0530

    Pagination for news and events

--- frontend/src/view/Events/Events.jsx
+++ frontend/src/view/Events/Events.jsx
@@ -6,15 +6,13 @@ import Grid from "@mui/material/Grid";
 import NewsCard from "../../components/NewsCard/NewsCard";
 
 const Item = styled(Paper)(({ theme }) => ({
-  // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  // ...theme.typography.body2,
-  // padding: theme.spacing(1),
-  // textAlign: 'center',
-  // color: theme.palette.text.secondary,
+  // ... (your Paper styling)
 }));
 
 export default function Events() {
   const [newsData, setNewsData] = React.useState([]);
+  const itemsPerPage = 4;
+  const [currentPage, setCurrentPage] = React.useState(1);
 
   React.useEffect(() => {
     async function fetchNewsData() {
@@ -30,11 +28,21 @@ export default function Events() {
     fetchNewsData();
   }, []);
 
+  const totalPages = Math.ceil(newsData.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, newsData.length);
+  const visibleNews = newsData.slice(startIndex, endIndex);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <h1 className="h1-heading">News and Events</h1>
       <Grid container spacing={2}>
-        {newsData.map((news, index) => (
+        {visibleNews.map((news, index) => (
           <Grid item xs={6} md={4} lg={3} key={index}>
             <Item>
               <NewsCard
@@ -53,6 +61,20 @@ export default function Events() {
           </Grid>
         ))}
       </Grid>
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            style={{
+              marginRight: "5px",
+              fontWeight: currentPage === index + 1 ? "bold" : "normal",
+            }}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </Box>
   );
 }
