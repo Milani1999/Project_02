6ce760f33a86b997be6af1adf0da5517e0fb77bc
Milani Yogeswaran <milaniyoges@gmail.com>
2023-08-17 10:22:50 +0530

    news and events popup

--- frontend/src/components/NewsCard/NewsCard.jsx
+++ frontend/src/components/NewsCard/NewsCard.jsx
@@ -1,29 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./NewsCard.css";
 
-const NewsCard = ({
-  ImgSrc,
-  ImgAlt,
-  title,
-  description,
-  btnText,
-  link}) => {
+const NewsCard = ({ ImgSrc, ImgAlt, title, description, btnText, onClick }) => {
   return (
-    <div className='card-container'>
+    <div className="card-container">
       {ImgSrc && ImgAlt && (
-        <img src={ImgSrc} alt={ImgAlt} className='card-img'/>
+        <img src={ImgSrc} alt={ImgAlt} className="card-img" />
       )}
-      {title && (
-        <h3 className='card-title'>{title}</h3>
-      )}
-      {description && (
-        <p className='card-desc'>{description}</p>
-      )}
+      {title && <h3 className="card-title">{title}</h3>}
+      {description && <p className="card-desc">{description}</p>}
       {btnText && (
-        <a href={link} className='card-btn'>{btnText}</a>
+        <button onClick={onClick} className="card-btn">
+          {btnText}
+        </button>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default NewsCard;
--- frontend/src/view/Events/Events.jsx
+++ frontend/src/view/Events/Events.jsx
@@ -4,15 +4,16 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import NewsCard from "../../components/NewsCard/NewsCard";
+import "./Events.css";
 
-const Item = styled(Paper)(({ theme }) => ({
-  // ... (your Paper styling)
-}));
+const Item = styled(Paper)(({ theme }) => ({}));
 
 export default function Events() {
   const [newsData, setNewsData] = React.useState([]);
   const itemsPerPage = 4;
   const [currentPage, setCurrentPage] = React.useState(1);
+  const [selectedNews, setSelectedNews] = React.useState(null);
+  const [showPopup, setShowPopup] = React.useState(false);
 
   React.useEffect(() => {
     async function fetchNewsData() {
@@ -38,43 +39,71 @@ export default function Events() {
     setCurrentPage(page);
   };
 
+  const handleCardClick = (news) => {
+    setSelectedNews(news);
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <h1 className="h1-heading">News and Events</h1>
-      <Grid container spacing={2}>
-        {visibleNews.map((news, index) => (
-          <Grid item xs={6} md={4} lg={3} key={index}>
-            <Item>
-              <NewsCard
-                ImgSrc={news.image}
-                ImgAlt={news.title}
-                title={news.title}
-                description={
-                  news.content.length > 200
-                    ? `${news.content.substring(0, 177)}...`
-                    : news.content
-                }
-                btnText="Read More"
-                link={`news/${news.id}`}
-              />
-            </Item>
-          </Grid>
-        ))}
-      </Grid>
-      <div>
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            style={{
-              marginRight: "5px",
-              fontWeight: currentPage === index + 1 ? "bold" : "normal",
-            }}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
-    </Box>
+    <div className="eventComponent">
+      <Box sx={{ flexGrow: 1 }}>
+        <h1 className="h1-heading">News and Events</h1>
+        <Grid container spacing={2}>
+          {visibleNews.map((news, index) => (
+            <Grid item xs={6} md={4} lg={3} key={index}>
+              <Item>
+                <NewsCard
+                  ImgSrc={news.image}
+                  ImgAlt={news.title}
+                  title={news.title}
+                  description={
+                    news.content.length > 200
+                      ? `${news.content.substring(0, 177)}...`
+                      : news.content
+                  }
+                  btnText="Read More"
+                  onClick={() => handleCardClick(news)}
+                />
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+        <div className="pagination-div">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              className="pagination"
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              style={{
+                marginRight: "5px",
+                fontWeight: currentPage === index + 1 ? "bold" : "normal",
+              }}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      </Box>
+      {showPopup && selectedNews && (
+        <div className="popup-news">
+          <div className="popup-inner-news">
+            <h2>{selectedNews.title}</h2>
+            <img
+              src={selectedNews.image}
+              alt={selectedNews.title}
+              className="popup-image"
+            />
+            <p>{selectedNews.content}</p>
+            <button className="btn btn-danger" onClick={closePopup}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
