0548cb3a6a484b8510151d1160c9840146fabaa3
Milani Yogeswaran <milaniyoges@gmail.com>
2023-08-20 23:15:19 +0530

    view subject and staff

--- frontend/src/components/Administrator/pages/Timetable.jsx
+++ frontend/src/components/Administrator/pages/Timetable.jsx
@@ -3,48 +3,70 @@ import axios from "axios";
 import "./Timetable.css";
 
 const TimeTable = () => {
-  const [timeTableData, setTimeTableData] = useState([]);
+  const periods = 8;
+  const weekdays = 5;
+
+  const [timetableData, setTimetableData] = useState([]);
+  const [grade, setGrade] = useState("");
 
   useEffect(() => {
-    fetchTimeTable();
-  }, []);
-
-  const fetchTimeTable = async () => {
-    try {
-      const response = await axios.get("/api/timetable");
-      setTimeTableData(response.data);
-    } catch (error) {
-      console.error("Error fetching time table data:", error);
+    if (grade !== "") {
+      axios
+        .get(`/api/timetable/grade/${grade}`)
+        .then((response) => {
+          setTimetableData(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching timetable data:", error);
+        });
     }
+  }, [grade]); // Include 'grade' as a dependency here
+
+  const getCellData = (weekday, period) => {
+    const matchingCell = timetableData.find(
+      (data) => data.weekday === weekday && data.period === period
+    );
+    if (matchingCell) {
+      return `${matchingCell.subject} - ${matchingCell.staff_name.join(", ")}`;
+    }
+    return "";
+  };
+
+  const handleGradeChange = (event) => {
+    setGrade(event.target.value);
   };
 
   return (
     <div className="time-table">
+      <div>
+        <label>Select Grade:</label>
+        <select value={grade} onChange={handleGradeChange}>
+          <option value="">Select</option>
+          {Array.from({ length: 11 }, (_, index) => (
+            <option key={index} value={index + 1}>
+              Grade {index + 1}
+            </option>
+          ))}
+        </select>
+      </div>
       <table>
         <thead>
           <tr>
-            <th>Periods</th>
-            {timeTableData.map((rowData, index) => (
-              <th key={index}>Day {rowData.weekday}</th>
-            ))}
+            <td></td>
+            <td>Monday</td>
+            <td>Tuesday</td>
+            <td>Wednesday</td>
+            <td>Thursday</td>
+            <td>Friday</td>
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }, (_, periodIndex) => (
-            <tr key={periodIndex}>
-              <td>Period {periodIndex + 1}</td>
-              {timeTableData.map((rowData, index) => {
-                const staffData = rowData.staff.find(
-                  (staff) => staff.period === periodIndex + 1
-                );
-                return (
-                  <td key={index}>
-                    {staffData
-                      ? `${staffData.subject} - ${staffData.staff_name}`
-                      : ""}
-                  </td>
-                );
-              })}
+          {Array.from({ length: periods }).map((_, period) => (
+            <tr key={period}>
+              <td>Period : {period + 1}</td>
+              {Array.from({ length: weekdays }).map((_, weekday) => (
+                <td key={weekday}>{getCellData(weekday + 1, period + 1)}</td>
+              ))}
             </tr>
           ))}
         </tbody>
