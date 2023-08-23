3de11e396122914c8b86b31e39bee57129269db2
Isharaherath <92168141+Isharaherath@users.noreply.github.com>
2023-08-23 12:30:52 +0530

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ import Dashboard from "./components/Administrator/pages/Dashboard";
 import EditTeacher from "./components/Administrator/pages/EditTeacher";
 import EditStudent from "./components/Administrator/pages/EditStudent";
 import Subject from "./components/Administrator/pages/Subject";
-import './assets/Style/styles.css'
+import "./assets/Style/styles.css";
 import Calender from "./components/Administrator/pages/Calender";
 import Timetable from "./components/Administrator/pages/Timetable";
 import Attendence from "./components/Administrator/pages/Attendence";
@@ -49,6 +49,7 @@ import StudentAttendance from "./components/Administrator/pages/StudentAttendanc
 import ViewStudentAttendance from "./components/Student/Pages/ViewStudentAttendance";
 import Gallery from "./components/section/Gallery";
 import EditGallery from "./components/Administrator/pages/EditGallery";
+
 function App() {
   return (
     <Router>
@@ -106,8 +107,8 @@ function App() {
             <Route path="Notices" element={<Notices />} />
             <Route path="Performance" element={<Performance />} />
             <Route path="Support" element={<Support />} />
-            <Route path="StaffAttendance" element={<StaffAttendance/>}/>
-            <Route path="StudentAttendance" element={<StudentAttendance/>}/>
+            <Route path="StaffAttendance" element={<StaffAttendance />} />
+            <Route path="StudentAttendance" element={<StudentAttendance />} />
             <Route path="EditGallery" element={<EditGallery/>}/>
           </Route>
 
@@ -121,21 +122,14 @@ function App() {
           >
             <Route index element={<Dashboard />} />
             <Route path="ProfilePage" element={<ProfilePage />} />
-            <Route path="TimeTableStd" element={<TimeTable />} />
+            <Route path="TimeTable" element={<TimeTable />} />
             <Route path="Marks" element={<Marks />} />
-            <Route path="ViewStudentAttendance" element={<ViewStudentAttendance />} />
+            <Route
+              path="ViewStudentAttendance"
+              element={<ViewStudentAttendance />}
+            />
             <Route path="ViewNotice" element={<SNotices />} />
-           <Route path="Scalender" element={<TCalendar />} />
-            {/* <Route path="EditTeacher" element={<EditTeacher />} />
-                <Route path="EditStudent" element={<EditStudent />} />
-
-                <Route path="Subject" element={<Subject />} />
-                <Route path="calender" element={<Calender />} />
-                <Route path="Timetable" element={<Timetable />} />
-                <Route path="Attendence" element={<Attendence />} />
-                <Route path="Grade" element={<Grade />} />
-                <Route path="Notices" element={<Notices />} />
-                <Route path="Performance" element={<Performance />} />  */}
+            <Route path="Scalender" element={<TCalendar />} />
           </Route>
         </Routes>
         <FooterRoutes />
