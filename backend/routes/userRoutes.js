e21991ffdfd4a4d743ee6dbb9603f409a6d0f756
Milani Yogeswaran <milaniyoges@gmail.com>
2023-10-26 22:39:19 +0530

    forgot password routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {authUser } = require('../controllers/userController')
+const { authUser } = require("../controllers/userController");
+const {
+  forgotPasswordController,
+} = require("../controllers/forgotPwdController");
 
-router.route('/login').post(authUser);
+router.route("/login").post(authUser);
+router.route("/forgot-password").post(forgotPasswordController);
 
 module.exports = router;
