e963ba08bb818f603654517511bf7a8452d6c17a
Milani Yogeswaran <milaniyoges@gmail.com>
2023-11-06 10:23:44 +0530

    host cancelled

--- frontend/src/components/section/Gallery.jsx
+++ frontend/src/components/section/Gallery.jsx
@@ -1,183 +1,83 @@
 import React, { useState, useEffect } from "react";
-import md5 from "crypto-js/md5";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
 
-// Update this import path to your context file
+import "./Gallery.css";
 
-const Payment = () => {
-  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const [paymentRecords, setPaymentRecords] = useState([]);
+function Gallery() {
+  const [imageList, setImageList] = useState([]);
 
   useEffect(() => {
-    const fetchPaymentRecords = async () => {
-      // Replace with actual logic to fetch payment records
-      // ...
+    fetchImages();
+  }, []);
 
-      // Load PayHere script
-      loadPayHereScript();
-    };
-
-    fetchPaymentRecords();
-  }, [selectedYear]);
-
-  const loadPayHereScript = () => {
-    // Load the PayHere script only if it's not already loaded
-    if (!window.payhere) {
-      const script = document.createElement("script");
-      script.src = "https://www.payhere.lk/lib/payhere.js";
-      script.async = true;
-      document.body.appendChild(script);
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get("/api/gallery/get");
+      console.log("Response",response);
+      setImageList(response.data);
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleYearChange = (event) => {
-    setSelectedYear(event.target.value);
-  };
-
-  // Generate an array for the months to display
-  const monthsToDisplay = Array.from(
-    {
-      length:
-        selectedYear === new Date().getFullYear()
-          ? new Date().getMonth() + 1
-          : 12,
-    },
-    (_, i) => new Date(0, i).toLocaleString("en-US", { month: "long" })
-  );
-
-  // Define merchant details (normally retrieved from a secure source or environment variables)
-  const merchantId = '1224567';
-  const merchantSecret = 'MjMzMzgzMjAwNjMxNjY5MDI3ODA0MjQzMjM3MzMxMTA1ODg1NjQzOA==';
-
-  // Function to generate the hash (this should ideally be done on the server)
-  const generateHash = (orderId, amount) => {
-    const hashString = `${merchantId}${orderId}${amount}LKR${md5(merchantSecret)
-      .toString()
-      .toUpperCase()}`;
-    return md5(hashString).toString().toUpperCase();
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <div>
-      <h2>Payment Records for {selectedYear}</h2>
-      <select value={selectedYear} onChange={handleYearChange}>
-        {Array.from(
-          { length: new Date().getFullYear() - 2021 + 1 },
-          (_, i) => new Date().getFullYear() - i
-        ).map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
+    <div className="GalleryComponent" id="GalleryComponent">
+      <div className="row">
+        <h1 className="h1-heading">Gallery</h1>
+      </div>
+      <Slider {...settings}>
+        {imageList.map((image) => (
+          <div key={image._id} className="galleryimg">
+            <img
+              className="img-fluid"
+              width={500}
+              src={image.image}
+              alt="Gallery"
+            />
+          </div>
         ))}
-      </select>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Month</th>
-            <th>Payment Date and Time</th>
-            <th>Review</th>
-            <th>Payment Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {monthsToDisplay.map((month) => {
-            const record = paymentRecords.find((r) => r.month === month);
-            const order_id = `SCHOOLFEES_${selectedYear}_${month}`;
-            const amount = "5000.00"; // Replace with the actual fee amount
-            const hash = generateHash(order_id, amount);
-
-            return (
-              <tr key={month}>
-                <td>{month}</td>
-                <td>{record ? record.paymentDateWithTime : "-"}</td>
-                <td>
-                  {record && record.pdfUrl ? (
-                    <a
-                      href={record.pdfUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Download PDF
-                    </a>
-                  ) : (
-                    "-"
-                  )}
-                </td>
-                <td>
-                  {record ? (
-                    "Paid"
-                  ) : (
-                    <form
-                      method="post"
-                      action="https://sandbox.payhere.lk/pay/checkout"
-                    >
-                      <input
-                        type="hidden"
-                        name="merchant_id"
-                        value={merchantId}
-                      />
-                      <input
-                        type="hidden"
-                        name="return_url"
-                        value="http://yourwebsite.com/return"
-                      />
-                      <input
-                        type="hidden"
-                        name="cancel_url"
-                        value="http://yourwebsite.com/cancel"
-                      />
-                      <input
-                        type="hidden"
-                        name="notify_url"
-                        value="http://yourwebsite.com/notify"
-                      />
-                      <input type="hidden" name="order_id" value={order_id} />
-                      <input
-                        type="hidden"
-                        name="items"
-                        value={`School Fees for ${month} ${selectedYear}`}
-                      />
-                      <input type="hidden" name="amount" value={amount} />
-                      <input type="hidden" name="currency" value="LKR" />
-                      <input
-                        type="hidden"
-                        name="first_name"
-                        value="Your_First_Name"
-                      />
-                      <input
-                        type="hidden"
-                        name="last_name"
-                        value="Your_Last_Name"
-                      />
-                      <input type="hidden" name="email" value="Your_Email" />
-                      <input
-                        type="hidden"
-                        name="phone"
-                        value="Your_Phone_Number"
-                      />
-                      <input
-                        type="hidden"
-                        name="address"
-                        value="Your_Address"
-                      />
-                      <input type="hidden" name="city" value="Your_City" />
-                      <input
-                        type="hidden"
-                        name="country"
-                        value="Your_Country"
-                      />
-                      <input type="hidden" name="hash" value={hash} />
-                      <button type="submit">Pay Now</button>
-                    </form>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      </Slider>
     </div>
   );
-};
+}
 
-export default Payment;
+export default Gallery;
